473fd1092e2fe8bb0adb6230c0924e4efec36282
Aga Daniuk <agdani@wp.pl>
2021-02-16 13:01:50 +0100

    Add OrderForm and OrderSummary

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './OrderSummary.scss';
+import PropTypes from 'prop-types';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+
+const OrderSummary = ({cost, options}) => {
+  return (
+    <h2 className={styles.component}>
+        Total: <strong>{formatPrice(calculateTotal(cost, options))}</strong>
+    </h2>
+  );
+};
+
+OrderSummary.propTypes = {
+  cost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
