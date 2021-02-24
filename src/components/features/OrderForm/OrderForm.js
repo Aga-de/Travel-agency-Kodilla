a1de524657bd6cd38bc2bac0e2cbb59787bc42e7
Aga Daniuk <agdani@wp.pl>
2021-02-24 13:48:56 +0100

    Finish OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,8 +5,48 @@ import OrderOption from '../OrderOption/OrderOption';
 import {Col, Row} from 'react-flexbox-grid';
 import pricing from '../../../data/pricing.json';
 import styles from './OrderForm.scss';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
-const OrderForm = ({tripCost, options, setOrderOption}) => {
+const sendOrder = (options, tripCost, tripName, tripId, tripCountry) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    tripName,
+    tripId,
+    tripCountryCode: tripCountry.alpha3Code,
+    ...options,
+    totalCost,
+
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  if(!options.name == '' && !options.contact == '') {
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  } else {
+    alert('Enter name and contact');
+  }
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, tripName, tripId, tripCountry}) => {
   return (
     <Row>
       <div className = {styles.options}>
@@ -21,12 +61,17 @@ const OrderForm = ({tripCost, options, setOrderOption}) => {
       <Col xs={12}>
         <OrderSummary cost={tripCost} options={options}/>
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCountry)}>Order now!</Button>
     </Row>
   );
 };
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  tripCountry: PropTypes.object,
+  tripCountryCode: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
 };
