9079a8287b199a76ac65573641a06aee59465e45
Aga Daniuk <agdani@wp.pl>
2021-03-01 13:19:08 +0100

    Add formatTime

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './HappyHourAd.scss';
 import PropTypes from 'prop-types';
+import {formatTime} from '../../../utils/formatTime';
 
 
 class HappyHourAd extends React.Component {
@@ -36,7 +37,7 @@ class HappyHourAd extends React.Component {
         <div className={styles.component}>
           <h3 className={styles.title}>{title}</h3>
           <div className={styles.promoDescription}>
-            {countdown > 23 * 60 * 60 ? promoDescription : countdown}
+            {countdown > 23 * 60 * 60 ? promoDescription : formatTime(countdown)}
           </div>
         </div>   
       );
