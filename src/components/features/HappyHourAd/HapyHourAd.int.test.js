7508710305d089c69166e0f2eb9222ec48924430
Aga Daniuk <agdani@wp.pl>
2021-03-02 15:46:03 +0100

    Fix HappyHourAd.int.test

--- src/components/features/HappyHourAd/HapyHourAd.int.test.js
+++ src/components/features/HappyHourAd/HapyHourAd.int.test.js
@@ -58,6 +58,10 @@ const checkDescriptionAfterTime = (time, delaySeconds, expectedDescription) => {
   });
 };
 
+beforeAll(() => {
+  const utilsModule = jest.requireActual('../../../utils/formatTime.js');
+  utilsModule.formatTime = jest.fn(seconds => seconds);
+});
 
 describe('Component HappyHourAd with mocked Date', () => {
   checkDescriptionAtTime('11:57:58', '122');
